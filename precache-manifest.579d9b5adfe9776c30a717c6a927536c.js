cd1f52f5078d88ae66e387459c0c2e30293c5218
stellie82 <kims82@gmail.com>
2020-01-23 12:32:11 -0800

    Updates

---
+++ precache-manifest.579d9b5adfe9776c30a717c6a927536c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed13c753802e19c8f549bb9fc32c3a7f",
+    "url": "/memoryClicker/index.html"
+  },
+  {
+    "revision": "e8b4acc856e326879093",
+    "url": "/memoryClicker/static/css/main.544a631d.chunk.css"
+  },
+  {
+    "revision": "e764febed043d74d7688",
+    "url": "/memoryClicker/static/js/2.d781a221.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/memoryClicker/static/js/2.d781a221.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e8b4acc856e326879093",
+    "url": "/memoryClicker/static/js/main.853a74ee.chunk.js"
+  },
+  {
+    "revision": "baeabe98f11e51255642",
+    "url": "/memoryClicker/static/js/runtime-main.94b85538.js"
+  },
+  {
+    "revision": "439ff6571bc76df9773f6f0feacb1fbf",
+    "url": "/memoryClicker/static/media/moon.439ff657.jpg"
+  },
+  {
+    "revision": "904152d12e6d4ab44dd3f97ba6056ffa",
+    "url": "/memoryClicker/static/media/space.904152d1.jpg"
+  }
+]);
